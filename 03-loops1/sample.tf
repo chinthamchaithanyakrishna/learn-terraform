d234f94e9d2698a9ba2d28d718e7ef1216e8c871
chaithanya <chaithanyack656@gmail.com>
2025-07-21 20:20:48 +0530

    learn ansible

---
+++ 03-loops1/sample.tf
@@ -0,0 +1,19 @@
+resource "null_resource" "dummy" {
+  count = 10
+}
+
+provider "aws" {}
+
+variable "instances" {
+  default = ["catalogue", "frontend"]
+}
+
+resource "aws_instance" "web" {
+  count = length(var.instances)
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
